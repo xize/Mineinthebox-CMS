8333dae2d56923d4d5353614a49331551d9ea4f5
xize <glucassen@live.nl>
2013-12-05 17:41:11 +0100

    added signup beginnings

--- account.php
+++ account.php
@@ -132,5 +132,4 @@ class account extends config {
 		}
 	}
 }
-
 ?>
---
+++ signup.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+class signup extends config {
+	
+	public function checkAlreadyLoggedIn() {
+		if(!empty($_SESSION['username']) || isset($_SESSION['username'])) {
+			header("Location: index.php");
+			return true;
+		}
+		return false;
+	}
+	
+	//checks specific on email patterns and we use the unsafe String to see
+	public function checkEmailRegex($mail) {
+		$low = strtolower($mail);
+		if(filter_var($low, FILTER_VALIDATE_EMAIL)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//returns when the email already is used!, returns false when the use doesn't exist on this email.
+	public function checkEmail($mailAdress) {
+		$low = stripcslashes(mysql_real_escape_string(strtolower($mailAdress)));
+		if($this->checkEmailRegex($mailAdress)) {
+			$sql = mysql_query("SELECT user FROM users WHERE email='" . $low . "'");
+			if(!$sql) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+}
+
+?>
